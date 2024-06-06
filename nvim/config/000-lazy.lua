76409f6d364d2a8f16030ca5ce78562feb6e895e
NerdyWoffy <hello@nerdywoffy.me>
2024-06-06 18:05:07 +0700

    feat(nvim): add formatters and indent line

--- nvim/config/000-lazy.lua
+++ nvim/config/000-lazy.lua
@@ -1,115 +1,124 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  {
-	"nvim-neo-tree/neo-tree.nvim",
-	keys = {
-	  { "<leader>ff", "<cmd>Neotree<cr>", desc = "NeoTree" },
-	  { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		keys = {
+			{ "<leader>ff", "<cmd>Neotree<cr>", desc = "NeoTree" },
+			{ "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup()
+		end,
 	},
-	dependencies = {
-	  "nvim-lua/plenary.nvim",
-	  "nvim-tree/nvim-web-devicons",
-	  "MunifTanjim/nui.nvim",
+	{
+		"ray-x/go.nvim",
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
 	},
-	config = function()
-	  require("neo-tree").setup()
-	end,
-  },
-  {
-	"ray-x/go.nvim",
-	dependencies = { 
-	"ray-x/guihua.lua",
-	"neovim/nvim-lspconfig",
-	"nvim-treesitter/nvim-treesitter",
+	{
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 	},
-	config = function()
-		require("go").setup()
-	end,
-	event = {"CmdlineEnter"},
-	ft = {"go", "gomod"},
-	build = ':lua require("go.install").update_all_sync()'
-  },
-  {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  {
-	"vim-airline/vim-airline",
-	lazy = false,
-	priority = 1000,
-	dependencies = {
-		{"vim-airline/vim-airline-themes"},
-		{"ryanoasis/vim-devicons"}, 
-	}
-  },
-  {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-  },
-  {
-	"mrjones2014/legendary.nvim",
-	version = "v2.13.9",
-	priority = 10000,
-	lazy = false,
-	dependencies = { "kkharji/sqlite.lua" }
-  },
-  {
-	"stevearc/dressing.nvim",
-	opts = {},
-  },
-  {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {
-	"theHamsta/nvim-dap-virtual-text",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio"
-	}
-  },
-  {
-	"akinsho/toggleterm.nvim",
-	keys = {
-	  { "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Toggleterm" },
+	{
+		"vim-airline/vim-airline",
+		lazy = false,
+		priority = 1000,
+		dependencies = {
+			{ "vim-airline/vim-airline-themes" },
+			{ "ryanoasis/vim-devicons" },
+		},
 	},
-	version = "*",
-	config = true
-  },
-  {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-	  "LazyGit",
-	  "LazyGitConfig",
-	  "LazyGitCurrentFile",
-	  "LazyGitFilter",
-	  "LazyGitFilterCurrentFile",
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
-	dependencies = {
-	  "nvim-lua/plenary.nvim",
+	{
+		"mrjones2014/legendary.nvim",
+		version = "v2.13.9",
+		priority = 10000,
+		lazy = false,
+		dependencies = { "kkharji/sqlite.lua" },
 	},
-	keys = {
-	  { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-	}
-  }
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		keys = {
+			{ "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Toggleterm" },
+		},
+		version = "*",
+		config = true,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 })
--- nvim/config/002-config.lua
+++ nvim/config/002-config.lua
@@ -1,34 +1,64 @@
 -- setup legendary
-require('legendary').setup({ extensions = { lazy_nvim = true } })
+require("legendary").setup({ extensions = { lazy_nvim = true } })
 
 -- setup telescope
-require('telescope').setup({
-  defaults = {
-	layout_strategy = 'vertical',
-	layout_config = {
-	  prompt_position = 'top',
-	  mirror = true,
-      vertical = { width = 0.85 },
-    },
-  },
+require("telescope").setup({
+	defaults = {
+		layout_strategy = "vertical",
+		layout_config = {
+			prompt_position = "top",
+			mirror = true,
+			vertical = { width = 0.85 },
+		},
+	},
 })
+
+-- setup ibl
+require("ibl").setup()
+
+-- setup formatter
+require("conform").setup({
+	formatters_by_ft = {
+		-- do not register go as it is using formatter that handled by `go.format` on go.nvim instead
+		lua = { "stylua" },
+		javascript = { { "prettierd", "prettier" } },
+		json = { "jq" },
+		yaml = { "yq" },
+		javascript = { { "prettierd", "prettier" } },
+	},
+})
+
+vim.keymap.set("n", "<leader>p", function()
+	require("conform").format()
+end)
 
 -- go.nvim - format before save
 local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
+	pattern = "*.go",
+	callback = function()
+		require("go.format").goimport()
+	end,
+	group = format_sync_grp,
 })
 
--- set tab to 4 space 
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- set tab to 4 space
 vim.opt.ts = 4
 vim.opt.sts = 4
 vim.opt.sw = 4
 
+-- smart tab and number
+vim.opt.smarttab = true
+vim.opt.number = true
+
 -- command pallette-alike commands
-vim.keymap.set('n', '<C-p>f', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<C-p>p', '<cmd>Telescope commands<cr>')
-vim.keymap.set('n', '<C-p>/', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set("n", "<C-p>f", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "<C-p>p", "<cmd>Telescope commands<cr>")
+vim.keymap.set("n", "<C-p>/", "<cmd>Telescope live_grep<cr>")
