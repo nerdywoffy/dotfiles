ea42f14bceb72644a4d28682d15faa7fb1c3150d
NerdyWoffy <hello@nerdywoffy.me>
2024-06-06 14:21:47 +0700

    feat(nvim): add lazygit.nvim to lazy deps

--- nvim/config/000-lazy.lua
+++ nvim/config/000-lazy.lua
@@ -95,5 +95,21 @@ require("lazy").setup({
 	},
 	version = "*",
 	config = true
+  },
+  {
+	"kdheepak/lazygit.nvim",
+	cmd = {
+	  "LazyGit",
+	  "LazyGitConfig",
+	  "LazyGitCurrentFile",
+	  "LazyGitFilter",
+	  "LazyGitFilterCurrentFile",
+	},
+	dependencies = {
+	  "nvim-lua/plenary.nvim",
+	},
+	keys = {
+	  { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+	}
   }
 })
