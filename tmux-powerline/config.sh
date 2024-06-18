60cc2599fdd6e386a849244fbdb71e7de5271864
NerdyWoffy <hello@nerdywoffy.me>
2024-06-18 17:16:29 +0700

    feat(tmux-powerline): move nowplaying file to `/tmp`

--- tmux-powerline/config.sh
+++ tmux-powerline/config.sh
@@ -213,7 +213,7 @@
 	# Music player to use. Can be any of {audacious, banshee, cmus, apple_music, itunes, lastfm, plexamp, mocp, mpd, mpd_simple, pithos, playerctl, rdio, rhythmbox, spotify, spotify_wine, file}.
 	export TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER="file"
 	# File to be read in case the song is being read from a file
-	export TMUX_POWERLINE_SEG_NOW_PLAYING_FILE_NAME="/Users/nerdywoffy/.config/tmux-powerline/nowplaying"
+	export TMUX_POWERLINE_SEG_NOW_PLAYING_FILE_NAME="/tmp/nowplaying"
 	# Maximum output length.
 	export TMUX_POWERLINE_SEG_NOW_PLAYING_MAX_LEN="32"
 	# How to handle too long strings. Can be {trim, roll}.
