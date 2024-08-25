ee658b57fb2b4833a7a5c7873bbdaaeb2ef7df52
NerdyWoffy <hello@nerdywoffy.me>
2024-08-25 13:45:32 +0700

    feat(nvim): add minimap
    i forgot why i ended up adding it to nvim

---
+++ nvim/config/004-mini.lua
@@ -0,0 +1,10 @@
+-- require("mini.animate").setup()
+require("mini.map").setup()
+vim.keymap.set("n", "<Leader>mc", MiniMap.close)
+vim.keymap.set("n", "<Leader>mf", MiniMap.toggle_focus)
+vim.keymap.set("n", "<Leader>mo", MiniMap.open)
+vim.keymap.set("n", "<Leader>mr", MiniMap.refresh)
+vim.keymap.set("n", "<Leader>ms", MiniMap.toggle_side)
+vim.keymap.set("n", "<Leader>mt", MiniMap.toggle)
+
+require("mini.cursorword").setup()
